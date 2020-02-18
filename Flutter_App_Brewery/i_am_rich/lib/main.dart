153098b11f13eaa2d45187fbd48ecfa8f7f2a5fa
Manthan Ladva <40834879+manthan-ladva@users.noreply.github.com>
2020-02-18 23:05:31 +0530

    change in main.dart also adding gifs

--- Flutter_App_Brewery/i_am_rich/lib/main.dart
+++ Flutter_App_Brewery/i_am_rich/lib/main.dart
@@ -6,10 +6,17 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("I Am Rich"),
+          title: Text("I Am Rich ..by Manthan",
+              style: TextStyle(color: Colors.red[800])),
           // backgroundColor: Color(0xFF00C853), //0xFF <- Pallet Code
           backgroundColor: Colors.yellow[600],
         ),
+        body: Center(
+          child: Image(
+              image: NetworkImage(
+                  'https://media.giphy.com/media/pYCZPDymIVjeo/giphy.gif')),
+        ),
+        //backgroundColor: Colors.black,
       ),
     ),
   );
