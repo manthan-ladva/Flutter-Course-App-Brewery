4f38d3ae596180733c0eebf41417984c3be4c312
Manthan Ladva <40834879+manthan-ladva@users.noreply.github.com>
2020-09-12 15:48:17 +0530

    Clima app Complete

---
+++ 10. clima/lib/scratch.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+void main() {
+  performTasks();
+}
+
+void performTasks() {
+  task1();
+  task2();
+  task3();
+}
+
+void task1() {
+  String result = 'task 1 data';
+  print('Task 1 complete');
+}
+
+void task2() {
+  Duration threesec = Duration(seconds: 3);
+  Future.delayed(threesec, () {
+    String result = 'task 2 data';
+    print('Task 2 complete');
+  });
+}
+
+void task3() {
+  String result = 'task 3 data';
+  print('Task 3 complete');
+}
